ad8580c863965466469bd9054e753de233e7702c
soozynn <81217456+soozynn@users.noreply.github.com>
2022-06-23 00:30:14 +0900

    fix: reuse in overlapping sections by replacing the ReservationSection with the FeatureSection

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -118,7 +118,6 @@ export default function Footer() {
   return (
     <FooterBackground>
       <FooterContainer>
-<<<<<<< HEAD
         <FirstRow>
           <EntrepreneurText>
             트리플 사업자 정보
@@ -162,55 +161,6 @@ export default function Footer() {
           </InformationWrapper>
         )}
 
-<<<<<<< HEAD
-=======
-
-=======
-        <div>
-          <FirstRow>
-            <EntrepreneurText>
-              트리플 사업자 정보
-              <ArrowIcon
-                src={underArrowSrc}
-                alt="arrow-icon"
-                onClick={handleClickButton}
-              />
-            </EntrepreneurText>
-            <ButtonsContainer>
-              <Button
-                location={"https://triple.guide/login?returnUrl=%2Fintro%2F"}
-              >
-                로그인
-              </Button>
-              <Button
-                location={
-                  "https://play.google.com/store/apps/details?id=com.titicacacorp.triple&pid=intro_web"
-                }
-                text={"트리플 앱"}
-                src={downloadIconSrc}
-              />
-            </ButtonsContainer>
-          </FirstRow>
-          {isClicked && (
-            <InformationWrapper>
-              <Information>
-                주식회사 트리플 | 대표 김연정, 최휘영
-                <br />
-                사업자 등록번호 581-87-00266
-                <br />
-                통신판매업 신고번호 2017-성남분당-0275
-                <br />
-                경기도 성남시 분당구 판교역로 14번길 16, 3층
-                <br />
-                항공, 숙소 및 투어·티켓 문의 1588-2539
-                <br />
-                help@triple-corp.com
-              </Information>
-            </InformationWrapper>
-          )}
-        </div>
->>>>>>> 1e6dcd2 (fix: modify the Icon link of Footer)
->>>>>>> 0e2f33c (fix: modify the Icon link of Footer)
         <WarningPhrase>
           (주) 트리플은 통신판매중개로서 통신판매의 당사자가 아니며 상품
           거래정보 및 거래등에 대해 책임을 지지 않습니다.
