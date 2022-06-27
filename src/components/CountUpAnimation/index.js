24b848aa81ea06512c6fb5f2e6557f4c1967d880
soozynn <81217456+soozynn@users.noreply.github.com>
2022-06-27 23:09:38 +0900

    fix: easeOutQuad replace with function

--- src/components/CountUpAnimation/index.js
+++ src/components/CountUpAnimation/index.js
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-const easeOutQuad = (time) => time * (2 - time);
-const frameDuration = 1000 / 60;
+function easeOutQuad(t) {
+  return t === 1 ? 1 : 1 - Math.pow(2, -10 * t);
+}
 
 export default function CountUpAnimation({ children }) {
-  const animationDuration = 1000;
-  const countNumber = parseInt(children, 10);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
+    const animationDuration = 1000;
+    const frameDuration = 1000 / 60;
     const totalFrames = Math.round(animationDuration / frameDuration);
     let frame = 0;
 
     const counter = setInterval(() => {
       frame++;
+      const countNumber = parseInt(children, 10);
       const progress = easeOutQuad(frame / totalFrames);
       setCount(countNumber * progress);
 
